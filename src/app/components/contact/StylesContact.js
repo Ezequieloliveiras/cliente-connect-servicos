fbfe39ea9e1d68bad8038bab893c7dbbae401c95
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-04-25 22:41:32 -0300

    adicionando propriedade de estilo

--- src/app/components/contact/StylesContact.js
+++ src/app/components/contact/StylesContact.js
@@ -5,6 +5,8 @@ import ForwardToInboxIcon from '@mui/icons-material/ForwardToInbox'
 import { Box, Card, Grid } from '@mui/material'
 
 const StyledBox = styled(Card)`
+
+&& {
     background-color: #f9f9f9;
     border-radius: 10px ;
     padding: 50px;
@@ -15,40 +17,55 @@ const StyledBox = styled(Card)`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+} 
 `
 const StyledBoxContacts = styled(Box)`
-    display: flex; 
+&& {
+  display: flex; 
+}
+
 `
 const StyledForwardToInboxIcon = styled(ForwardToInboxIcon)`
-    margin-right: 10px;
-    color: #0f3464;
+&&{
+  margin-right: 10px;
+  color: #0f3464;
+}
 `
 const StyledWhatsAppIcon = styled(WhatsAppIcon)`
-    margin-right: 10px;
-    color: #25d366;
+&& {
+  margin-right: 10px;
+  color: #25d366;
+}
 `
 const StyledBanner = styled(Image)`
+&& {
   position: absolute;
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
 `
 const StyledContainerBanner = styled(Grid)`
-    height: 300px;
-    width: 100%;
-    overflow: hidden;
-    position: relative;
-  
+&& {
+  height: 300px;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+}
 `
 const StyledContainer = styled.div`
+&& {
   display:flex;
   flex-direction:column;
   align-content:center;
   justify-content:center;
+}
 `
 const StyledDivCard = styled.div`
+&& {
   display:flex;
   justify-content:center;
+}
 `
 export {
     StyledDivCard,
